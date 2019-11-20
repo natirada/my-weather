4a8eb8f415fd2b91f2d23ccd19df159f2cde882b
natirada <48950693+natirada@users.noreply.github.com>
2019-11-20 05:27:25 +0200

    update token

--- src/axios-weather.js
+++ src/axios-weather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
     baseURL: 'https://dataservice.accuweather.com/',
     params: {
-        apikey: 'Z4KPevJp0MDfG0b5GDxnJPnda24ExpSS'
+        apikey: 'ItHczBnBAjcPcjRSCeS4AqcbLGDFAHvq'
     }
 });
 
